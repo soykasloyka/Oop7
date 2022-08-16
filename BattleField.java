e3be7f5bde373ecd3675461a1eaa81e5b2495a73
soykasloyka <petr.emelianov@icloud.com>
2022-08-16 13:42:04 +0300

    battleField update

--- BattleField.java
+++ BattleField.java
@@ -6,33 +6,39 @@ public class BattleField {
    public static String galavirAttack = "Enemy Strikes";
    public static String galavirDestroy = "Enemy ship destroyed";
 
-    public static void main(String[] args) {
-        GalavirXIII galavir = new GalavirXIII();
-        Nimrod nimrod = new Nimrod();
+   public static void main(String[] args) {
+       GalavirXIII galavir = new GalavirXIII();
+       Nimrod nimrod = new Nimrod();
 
-        do {
-            System.out.println(galavirAttack);
-            nimrod.defend(galavir.attack());
-        } while (isNimrodAlive(nimrod));
-    }
+       do {
+           System.out.println(galavirAttack);
+           nimrod.defend(galavir.attack());
+           if (isNimrodAlive(nimrod)) {
+               System.out.println(nimrodAttack);
+               galavir.defend(nimrod.attack());
+           } else {
+               break;
+           }
+       } while (isGalavirAlive(galavir));
+   }
 
-    public static boolean isNimrodAlive(Nimrod nimrod) {
-        if (nimrod.health > 0) {
-            System.out.println(nimrodDefence);
-            return true;
-        } else {
-            System.out.println(nimrodDestroy);
-            return false;
-        }
-    }
+   public static boolean isNimrodAlive(Nimrod nimrod) {
+       if (nimrod.health > 0) {
+           System.out.println(nimrodDefence);
+           return true;
+       } else {
+           System.out.println(nimrodDestroy);
+           return false;
+       }
+   }
 
-    public static boolean isGalavirAlive(GalavirXIII galavir) {
-        if (galavir.health > 0) {
-            System.out.println(galavirDefence);
-            return true;
-        } else {
-            System.out.println(galavirDestroy);
-            return false;
-        }
-    }
+   public static boolean isGalavirAlive(GalavirXIII galavir) {
+       if (galavir.health > 0) {
+           System.out.println(galavirDefence);
+           return true;
+       } else {
+           System.out.println(galavirDestroy);
+           return false;
+       }
+   }
 }
