762542a509082de037ab6269ce4273ed048fccff
soykasloyka <petr.emelianov@icloud.com>
2022-08-16 13:39:28 +0300

    v3

---
+++ BattleField.java
@@ -0,0 +1,38 @@
+public class BattleField {
+   public static String nimrodDefence = "Enemy attack repulsed";
+   public static String nimrodAttack = "Counter";
+   public static String nimrodDestroy = "Enemy hit failed";
+   public static String galavirDefence = "Our attack was successfully repelled by the enemy";
+   public static String galavirAttack = "Enemy Strikes";
+   public static String galavirDestroy = "Enemy ship destroyed";
+
+    public static void main(String[] args) {
+        GalavirXIII galavir = new GalavirXIII();
+        Nimrod nimrod = new Nimrod();
+
+        do {
+            System.out.println(galavirAttack);
+            nimrod.defend(galavir.attack());
+        } while (isNimrodAlive(nimrod));
+    }
+
+    public static boolean isNimrodAlive(Nimrod nimrod) {
+        if (nimrod.health > 0) {
+            System.out.println(nimrodDefence);
+            return true;
+        } else {
+            System.out.println(nimrodDestroy);
+            return false;
+        }
+    }
+
+    public static boolean isGalavirAlive(GalavirXIII galavir) {
+        if (galavir.health > 0) {
+            System.out.println(galavirDefence);
+            return true;
+        } else {
+            System.out.println(galavirDestroy);
+            return false;
+        }
+    }
+}
